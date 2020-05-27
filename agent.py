fd2ce974199bfb12d10489e175d439ef1aba2b39
Marek Kancir <kancir.marek@gmail.com>
2020-05-27 13:00:34 +0200

    add act(), update buffer

--- agent.py
+++ agent.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import src
+import numpy as np
 
 class Agent():
     def __init__(self, state_size, action_size):
@@ -11,3 +12,16 @@ class Agent():
         self.epsilon_min = 0.01
         self.epsilon_decay = 0.995
         self.model = src.create_model(state_size, action_size)
+
+    def update_replay_buffer(self, state, action, reward, next_state, done):
+        self.memory.store(state, action, reward, next_state, done)
+        
+    def act(self, state):
+#        uses epsilon-greedy
+        if np.random.rand()<=self.epsilon:
+            return np.random.choice(self.a_size)
+        else:
+            act_values = self.model.predict(state)
+            return np.argmax(act_values[0])
+        
+    
